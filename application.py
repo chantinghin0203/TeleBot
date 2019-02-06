c362a4985c0951d66c3b02bc5c8c5aa17a84b2ee
chantinghin0203 <chantinghin0203@gmail.com>
2019-02-06 12:55:26 +0800

    Updated

--- application.py
+++ application.py
@@ -5,20 +5,25 @@ from telegram.ext import Updater
 
 import os
 
-TOKEN = "TOKEN"
+TOKEN = "786280876:AAE7S9sEdzmnFcxff4otomvtYicy7JL8kuw"
 PORT = int(os.environ.get('PORT', '8443'))
 
-
-updater = Updater(token='786280876:AAE7S9sEdzmnFcxff4otomvtYicy7JL8kuw')
+updater = Updater(token=TOKEN)
 dispatcher = updater.dispatcher
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 
 def start(bot, update: Update):
-    bot.send_message(chat_id=update.message.chat_id, text=f"I'm a bot, please talk to me! {update.effective_user['first_name']}")
+    bot.send_message(chat_id=update.message.chat_id,
+                     text=f"I'm a bot, please talk to me! {update.effective_user['first_name']}")
 
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
-updater.start_polling()
+
+updater.start_webhook(listen="0.0.0.0",
+                      port=PORT,
+                      url_path=TOKEN)
+updater.bot.set_webhook("https://tele-bot-dummy.herokuapp.com/" + TOKEN)
+updater.idle()
