988f0461701163743353d4b044d0096ccf86f2b5
chantinghin0203 <chantinghin0203@gmail.com>
2019-02-06 15:52:12 +0800

    Updated

--- teleBot/domain/service/CommandLineService.py
+++ teleBot/domain/service/CommandLineService.py
@@ -4,4 +4,4 @@ from telegram import Update
 class CommandLineService:
     def start_service(self, bot, update: Update):
         bot.send_message(chat_id=update.message.chat_id,
-                         text=f"I'm a bot, please talk to me! {update.effective_user['first_name']}")
+                         text=f"I'm a dummy bot, please talk to me! {update.effective_user['first_name']}")
