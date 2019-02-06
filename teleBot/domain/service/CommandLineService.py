04d8771329cad346f5e883ccec457148da30d5a2
chantinghin0203 <chantinghin0203@gmail.com>
2019-02-07 02:34:04 +0800

    Updated

--- teleBot/domain/service/CommandLineService.py
+++ teleBot/domain/service/CommandLineService.py
@@ -1,7 +1,14 @@
-from telegram import Update
+from telegram import Update, Bot
 
 
 class CommandLineService:
-    def start_service(self, bot, update: Update):
+    def start_service(self, bot: Bot, update: Update):
         bot.send_message(chat_id=update.message.chat_id,
                          text=f"I'm a dummy bot, please talk to me! {update.effective_user['first_name']}")
+
+    def start_callback_service(self, bot: Bot, update: Update):
+        query = update.callback_query
+
+        bot.edit_message_text(text="CallBack!!!!",
+                              chat_id=query.message.chat_id,
+                              message_id=query.message.message_id)
